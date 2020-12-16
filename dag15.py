f69f6dd7f30fe5d35e90bda40339b21516643dff
Ove <ove.kristian.jorgensen@gmail.com>
2020-12-16 14:20:44 +0100

    dag 15 :snowman:

--- dag15.py
+++ dag15.py
@@ -1,15 +1,12 @@
 import requests
 
-ordbok = requests.get('https://julekalender-backend.knowit.no/challenges/15/attachments/wordlist.txt').text.split('\n')
-ordpar = requests.get('https://julekalender-backend.knowit.no/challenges/15/attachments/riddles.txt').text.split('\n')
-ordpar.pop() #remove trailing whitespace
+ordbok = set(requests.get('https://julekalender-backend.knowit.no/challenges/15/attachments/wordlist.txt').text.split('\n'))
+ordpar = requests.get('https://julekalender-backend.knowit.no/challenges/15/attachments/riddles.txt').text.split('\n')[:-1]
 
-ordbok = {x for x in ordbok}
 limord = set()
-
 for word in ordbok:
     for par in ordpar:
-        first, second = par.replace(' ', '').split(',')
+        first, second = par.split(', ')
         if first+word in ordbok and word+second in ordbok:
             limord.add(word)
     
